2bef49f7c1c316ae32ebaffcadf887780f318eed
ctlott <CTLott@student.naperville203.org>
2017-05-05 09:18:36 -0500

    edits friday

--- Question.java
+++ Question.java
@@ -1,4 +1,10 @@
 public class Question
 {
+   private People[][] peopleArray = new People[][];
+   private String attribute;
+   private String choice;
+   public Question(People[][] array, String attribute){
+       peopleArray = array;
+    }
    
 }
