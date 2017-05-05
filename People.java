0021cb6e73a4ec7b1b633910241d90ec68f30714
Alexa Buko <asbukowski@stu.naperville203.org>
2017-05-05 09:17:55 -0500

    People
    Making variables all day every day

--- People.java
+++ People.java
@@ -1,4 +1,11 @@
 public class People
 {
-
+    private String eyes, hair, shirt, name;
+    private int x, y, year;
+    private char gender;
+    //private picture;
+    
+    public People(String e, String h, String s, String n){
+        
+    }
 }
