e4819b17657b216ab4e602e80e5c9da524183bb3
Alexa Buko <asbukowski@stu.naperville203.org>
2017-05-09 10:31:37 -0500

    ugh
    swegawgwg

--- Client.java
+++ Client.java
@@ -1,4 +1,31 @@
 public class Client
 {
    
+    static People[][] makeList(){
+        People[][] board = new People[4][6];
+        board[0][] = new People(
+        board[0][] = new People(
+        board[0][] = new People(
+        board[0][] = new People(
+        board[0][] = new People(
+        board[0][] = new People(
+        board[1][] = new People(
+        board[1][] = new People(
+        board[1][] = new People(
+        board[1][] = new People(
+        board[1][] = new People(
+        board[1][] = new People(
+        board[2][] = new People(
+        board[2][] = new People(
+        board[2][] = new People(
+        board[2][] = new People(
+        board[2][] = new People(
+        board[2][] = new People(
+        board[3][] = new People(
+        board[3][] = new People(
+        board[3][] = new People(
+        board[3][] = new People(
+        board[3][] = new People(
+        board[3][] = new People(
+    }
 }
