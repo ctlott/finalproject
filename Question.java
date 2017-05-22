fcd0e52b7ed7bc57daea419bcd0eaa8d750f45bb
ctlott <CTLott@student.naperville203.org>
2017-05-22 10:50:58 -0500

    asegtawegt

--- Question.java
+++ Question.java
@@ -1,8 +1,9 @@
-
+//imports
 import java.util.*;
 
 public class Question
 {
+   //creates all variables necessary
    private People person;
    public static int attribute;
    private int choice;
@@ -11,14 +12,22 @@ public class Question
    private boolean truefalse;
    Scanner Keyboard = new Scanner(System.in);
    
+   /**
+    * @param the person who is being asked about
+    */
    public Question(People peeps){
+       //sets instance vars
        person = peeps;
-       System.out.println("\n\n1. Graduation Year\n2. Hair Color\n3. Shirt Color\n4. Gender\n5. Ethnicity\n6. Has Glasses\n7. Guess Who!");
        
-       System.out.print("\nEnter an intenger for an attribute to guess about: ");
+       //prints out the menu
+       System.out.println("\n\n1. Graduation Year\n2. Hair Color\n3. Shirt Color\n4. Gender\n5. Ethnicity\n6. Has Glasses\n7. Guess Who!");
+       System.out.print("\nEnter an integer for an attribute you want to guess about: ");
+       
+       //sets the attribute to the next user input
        attribute = Keyboard.nextInt();
        System.out.println();
        
+       //for each integer, takes the user to each respective method
        if (attribute == 1)
             returnfinal = guessGradYear();
        else if (attribute == 2)
@@ -32,15 +41,21 @@ public class Question
        else if (attribute == 6)
             returnfinal = guessGlasses();   
        else if (attribute == 7){
+            //if they guess correctly, the variable guessedCorrectlyQuestion is set to true
             returnfinal = guessWho();
             if (returnfinal == true)
                 this.guessedCorrectlyQuestion = true;
         }
        else {
+            //catches invalid entries 
             System.out.println("Invalid entry");
             returnfinal = false;
         }
     }
+   /**
+    * lets the user guess the grad year
+    * @ return whether their guess was true or false (right or wrong)
+    */
    public boolean guessGradYear(){
            System.out.print("Enter the graduation year you want to guess: ");
            choice = Keyboard.nextInt();
@@ -51,6 +66,10 @@ public class Question
                 truefalse = false;
            return truefalse;
        }
+   /**
+    * lets the user guess the hair color
+    * @ return whether their guess was true or false (right or wrong)
+    */
    public boolean guessHairColor(){    
            System.out.println("\n\n1. Black\n2. Brown\n3. Dirty Blonde\n4. Bleach Blonde\n");
            System.out.print("Enter an integer for the hair color you want to guess: ");
@@ -62,6 +81,10 @@ public class Question
                 truefalse = false;
            return truefalse;
         }
+   /**
+    * lets the user guess the shirt color
+    * @ return whether their guess was true or false (right or wrong)
+    */
    public boolean guessShirtColor(){       
            System.out.println("\n1. Red\n2. Blue\n3. Black\n4. Grey\n5. Pattern\n");
            System.out.print("Enter an integer for the shirt color you want to guess: ");
@@ -73,6 +96,10 @@ public class Question
                 truefalse = false;
            return truefalse;
         }
+   /**
+    * lets the user guess the gender
+    * @ return whether their guess was true or false (right or wrong)
+    */
    public boolean guessGender(){
            System.out.print("Enter a 1 to guess boy, and a 2 to guess girl: ");
            choice = Keyboard.nextInt();
@@ -83,6 +110,10 @@ public class Question
                 truefalse = false;
            return truefalse;
         }
+   /**
+    * lets the user guess the ethnicity
+    * @ return whether their guess was true or false (right or wrong)
+    */
    public boolean guessEthnicity(){
            System.out.print("Enter a 1 to guess asian, and a 2 to guess white: ");
            choice = Keyboard.nextInt();
@@ -93,6 +124,10 @@ public class Question
                 truefalse = false;
            return truefalse;
         }
+   /**
+    * lets the user guess whether they have glasses
+    * @ return whether their guess was true or false (right or wrong)
+    */
    public boolean guessGlasses(){
            System.out.print("Enter a 1 to guess glasses, and a 2 to guess no glasses: ");
            choice = Keyboard.nextInt();
@@ -103,6 +138,10 @@ public class Question
                 truefalse = false;
            return truefalse;
         }
+   /**
+    * lets the user guess the person's name
+    * @ return whether their guess was true or false (right or wrong)
+    */
    public boolean guessWho(){
            System.out.print("Enter the name you want to guess: ");
            String str = Keyboard.next();
@@ -114,11 +153,20 @@ public class Question
            
            return truefalse;
         }
+   /**
+    * gets the result of the question method
+    * @ return whether their guess was true or false (right or wrong)
+    */
    public boolean getBoolean(){
            return this.returnfinal;
     }
+   /**
+    * returns the var that contains whether they guessed correctly in the guessWho method
+    * @ return whether their guess was true or false (right or wrong)
+    */
    public boolean getGuessedCorrectly(){
        return guessedCorrectlyQuestion;
     }
-    }
+
+    }//end of class
 
