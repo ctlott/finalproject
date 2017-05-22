a7a06149a75ef0709b50ed612060e386c17f0e07
ctlott <CTLott@student.naperville203.org>
2017-05-22 11:04:07 -0500

    zdg

--- Player.java
+++ Player.java
@@ -1,13 +1,20 @@
+//imports
 import java.util.*;
 
 public class Player
 {
+    //creates the people board
     public People[][] board;
+    //creates the person the user needs to guess
     private People chosenOne;
+    //other vars
     public static boolean guessedCorrectlyPlayer;
     private int Playernum;
     Scanner Keyboard = new Scanner(System.in);
     
+    /**
+     * @param the board of people and their attributes, and the number of the current player
+     */
     public Player(People[][] myBoard, int number){
         board = myBoard;
         guessedCorrectlyPlayer = false;
@@ -23,21 +30,39 @@ public class Player
         Playernum = number;
       
     }
+    /**
+     * returns the board of people and their attributes
+     */
     public People[][] getBoard(){
         return board;
     }
+    /**
+     * returns the person the user needs to guess
+     */
     public People getChosenOne(){
         return chosenOne;
     }
+    /**
+     * returns the number of the current player
+     */
     public int getPlayerNum(){
         return Playernum;
     }
+    /**
+     * returns whether the user guessed correctly
+     */
     public boolean guessedRight(){
         return guessedCorrectlyPlayer;
     }
+    /**
+     * if the user guesses the chosen one, this method is called
+     */
     public void setTrue(){
         guessedCorrectlyPlayer = true;
     }
+    /**
+     * returns the current player number
+     */
     public String toString(){
         return (" " + Playernum + " ");
     }
--- Turn.java
+++ Turn.java
@@ -1,54 +1,76 @@
+//imports
 import java.util.*;
 import java.io.*;
 
 /**
- * Write a description of class Game here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * The turn class controls each turn and uses the question class and takes the current player as a parameter
  */
 public class Turn
 {
+    //creates necessary vars
     Player currentplayer;
     People currentTarget;
     public static boolean guessedCorrectlyTurn = false;
     public static Scanner Keyboard = new Scanner(System.in);
+    /**
+     * constructor
+     * @param the current player, and the target person who the player needs to guess
+     */
     public Turn( Player currentplay, People targetPerson) throws IOException{
+        //sets all necessary variables
         currentTarget = targetPerson;
         currentplayer = currentplay;
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        
+        //creates a bunch of new lines so each player cannot see what the player before them did
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         System.out.println(currentplayer + ", it is your turn!");
         
         Displayer.main(currentplayer.getBoard());
         this.questionAnswer();
+        
+        //if the person guessed the person correctly they aren't prompted to remove players
         if(currentplayer.guessedRight() == false){
             this.remove();
         }
     }
+    /**
+     * lets the user know whether they guessed correctly YES or NO
+     */
     public void questionAnswer(){
+        //creates a new question
         Question newQ = new Question(currentTarget);
+        //if they did the Guess Who option, if they are correct, then the variable guessedCorrectlyTurn is set to true
         if (newQ.attribute == 7){
             if (newQ.getGuessedCorrectly() == true){
                 currentplayer.setTrue();
                 this.guessedCorrectlyTurn = true;
             }}
+        
+        //lets the user know whether their guess was right or wrong
         if (newQ.getBoolean() == false)
             System.out.println("The answer to that question is NO");
         else
             System.out.println("The answer to that question is YES");
     }
+    /**
+     * removes the people from the display board or makes them re-display
+     */
     public void remove(){
         int x = -1;
         int y = -1;
+        //prompts the user
         System.out.println("\nEnter the x and y location of the people you want to remove.");
         System.out.println("To reflip someone, enter the location again.");
         System.out.println("When you are done removing, enter 0.");
+        
+        //continually asks which people to remove until the user enters 0
         while(x != 0){
             System.out.print("\nX location: ");
             x = Keyboard.nextInt();
             System.out.print("Y location: ");
             y = Keyboard.nextInt();
             
+            //removes the person from the display or flips them back if they made a mistake
             if(x != 0 && y != 0){
                 currentplayer.getBoard()[y-1][x-1].flip();
                 if(currentplayer.getBoard()[y-1][x-1].getDisplayMe() == false){
